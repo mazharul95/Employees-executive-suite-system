f91812d712a13c8b68575c3b8bfe38fa3dd98c9a
mazharul95 <mipiyas07@gmail.com>
2022-04-20 05:48:45 +0600

    departments CRUD directive completed

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Backend\ChangePasswordController;
 use App\http\Controllers\Backend\CountryController;
 use App\http\Controllers\Backend\StateController;
 use App\http\Controllers\Backend\CityController;
+use App\http\Controllers\Backend\DepartmentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,4 +29,5 @@ Route::resource('users', UserController::class);
 Route::resource('countries', CountryController::class);
 Route::resource('states', StateController::class);
 Route::resource('cities', CityController::class);
+Route::resource('departments', DepartmentController::class);
 Route::post('users/{user}/change-password', [ChangePasswordController::class, 'change_password'])->name('users.change.password');
